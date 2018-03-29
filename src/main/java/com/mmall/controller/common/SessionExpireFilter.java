3dd39a7e70dcb4b21c0cdcd74aa0144f3ab6554f
Duruonanni <kate_2012@outlook.com>
2018-03-29 21:45:15 +0800

    commit add sessionExpireFilter

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,34 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CooKieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String loginToken = CooKieUtil.readLoginToken(httpServletRequest);
+        if(StringUtils.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr,User.class);
+            if(user != null) {
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {}
+}
