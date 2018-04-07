8a7c7d77099e15572fbbddd7e175d7af5a24fd9b
Duruonanni <kate_2012@outlook.com>
2018-04-07 10:41:34 +0800

    commit springmvc restful update

--- src/main/java/com/mmall/controller/portal/ProductController.java
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
 import com.mmall.service.IProductService;
 import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product")
@@ -23,6 +22,12 @@ public class ProductController {
         return iProductService.getProductDetail(productId);
     }
 
+    @RequestMapping(value = "/{productId}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTful(@PathVariable Integer productId){
+        return iProductService.getProductDetail(productId);
+    }
+
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<PageInfo> list
@@ -32,6 +37,46 @@ public class ProductController {
              @RequestParam(value = "pageSize",defaultValue = "10")Integer pageSize,
              @RequestParam(value = "orderBy",defaultValue = "desc")String orderBy) {
         return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+    //http://tomcat.imooc.com/product/keyword/手机/1/10/price_asc
+    @RequestMapping(value="/keyword/{keyword}/{pageNum}/pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful
+            (@PathVariable(value = "keyword")String keyword,
+             @PathVariable(value = "pageNum")Integer pageNum,
+             @PathVariable(value = "pageSize")Integer pageSize,
+             @PathVariable(value = "orderBy")String orderBy) {
+        if(pageNum == null) {
+            pageNum = 1;
+        }
+        if(pageSize == null) {
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    //http://tomcat.imooc.com/product/categoryId/100012/1/10/price_asc
+    @RequestMapping(value="/categoryId/{categoryId}/{pageNum}/pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful
+            (@PathVariable(value = "categoryId")Integer categoryId,
+             @PathVariable(value = "pageNum")Integer pageNum,
+             @PathVariable(value = "pageSize")Integer pageSize,
+             @PathVariable(value = "orderBy")String orderBy) {
+        if(pageNum == null) {
+            pageNum = 1;
+        }
+        if(pageSize == null) {
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(null,categoryId,pageNum,pageSize,orderBy);
     }
 
 }
